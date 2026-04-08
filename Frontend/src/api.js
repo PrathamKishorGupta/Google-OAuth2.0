33590fb56c9c23957f96a527dfcbdfb592355985
Pratham Kishor Gupta <prathamkishorgupta@gmail.com>
2026-04-08 22:24:04 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL : 'http://localhost:8080/auth'
+    baseURL : 'https://google-oauth2-0-ba.onrender.com/auth'
 });
 
 export const googleAuth = (code) => api.get(`/google?code=${code}`);
